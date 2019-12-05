7dc050af52acc41cc1413c9de29a9b7392fba505
awd28 <58564181+awd28@users.noreply.github.com>
2019-12-06 04:03:19 +0900

    Create ElevatorTest.java

---
+++ ElevatorTest.java
@@ -0,0 +1,52 @@
+class Elevator{
+
+	int floor;
+	boolean isOpen;
+	
+	void open(){
+		System.out.println("문이 열립니다.");
+		isOpen = true;
+	}
+	
+	void close(){
+		System.out.println("문이 닫힙니다.");
+		isOpen = false;
+	}
+	
+	void move(int n){
+		close();//문을 닫고
+
+		if(n>=-2&&n<=10){//n이 유효한 값일 경우 n층으로 이동	
+			
+			if(n ==0)	System.out.println("로비층으로 이동합니다.");
+			else if(n >0) System.out.println(n+"층으로 이동합니다.");
+			else  System.out.println("지하"+(-1*n)+"층으로 이동합니다.");
+			floor = n;
+		}
+		else{//n이 유효하지 않은 값인 경우 오류 메시지 출력 후 메소드 종료
+			System.out.println(n+"층은 없습니다. 층을 다시 입력해주세요. ");
+			return;
+		}
+		
+		report();//도착 후 현재 층 표시
+		
+		open();//문 열기
+	}
+	
+	void report(){
+		if(floor ==0)	System.out.println("로비층입니다.");
+		else if(floor>0) System.out.println(floor+"층입니다.");
+		else  System.out.println("지하"+(-1*floor)+"층입니다.");
+	}
+}
+
+class ElevatorTest{
+	public static void main(String [] args){
+		Elevator e1 = new Elevator();
+		Elevator e2 = new Elevator();
+		
+		e1.move(3);
+		
+		
+	}
+}
